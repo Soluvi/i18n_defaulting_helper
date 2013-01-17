89fb4d90dabcf7d7829eef7c3ad491fbb5e7968d
Mart Karu <karu@metal.ee>
2013-01-17 14:11:40 +0200

    Better Rails overriding

--- lib/i18n_defaulting_helper/translation_helper.rb
+++ lib/i18n_defaulting_helper/translation_helper.rb
@@ -1,15 +1,19 @@
 module I18nDefaultingHelper
   module ViewHelpers
 
-    alias_method :original_translate, :translate
-    def translate(key, options = {})
-      begin
-        original_translate(key, options.merge(raise: I18n::MissingTranslationData))
-      rescue I18n::MissingTranslationData
-        original_translate(key.gsub(/^\./, ''), options.merge(scope: :defaults))
+    def self.included(base)
+      ActionView::Helpers::TranslationHelper.module_eval do
+        alias_method :original_translate, :translate
+        def translate(key, options = {})
+          begin
+            original_translate(key, options.merge(raise: I18n::MissingTranslationData))
+          rescue I18n::MissingTranslationData
+            original_translate(key.gsub(/^\./, ''), options.merge(scope: :defaults))
+          end
+        end
+        alias_method :t, :translate
       end
     end
-    alias_method :t, :translate
 
   end
 end
