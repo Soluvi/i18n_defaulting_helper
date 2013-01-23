5f03ae5cf56789df75a9dfa13518fa18961089ba
Mart Karu <karu@metal.ee>
2013-01-23 17:25:16 +0200

    Simplified defaulting

--- lib/i18n_defaulting_helper/translation_helper.rb
+++ lib/i18n_defaulting_helper/translation_helper.rb
@@ -6,17 +6,11 @@ module I18nDefaultingHelper
 
         alias_method :original_translate, :translate
         def translate(key, options = {})
+          default_key = "defaults.%s" % key.gsub(/^\./, '')
           options.merge!(raise: I18n::MissingTranslationData)
-          begin
-            return original_translate(key, options)
-          rescue I18n::MissingTranslationData => e
-            original_exception = e
-          end
-          begin
-            return original_translate(key.gsub(/^\./, ''), options.merge(scope: :defaults))
-          rescue I18n::MissingTranslationData
-            raise original_exception
-          end
+          default = original_translate(default_key, options) rescue nil
+          options.merge!(default: default) if default
+          original_translate(key, options)
         end
         alias_method :t, :translate
 
